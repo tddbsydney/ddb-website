a0131c5a2558d9f94e9c0d6d9d6df6fb0953fdb2
CircleCI AutoBuilder <Technical@ddb.com.au>
2017-02-21 06:51:51 +0000

    Automated CircleCI rebuild at 05:51:51 PM (21-02-2017, AEDT).

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,7 @@ require("./base/template");
 /* empty block */
 
 // components
+var Header = require("./components/header.component");
 var PromoVideo = require("./components/promo-video.component");
 
 // controllers
@@ -57,7 +58,8 @@ console.log(CONFIG);
     // ---------------------------------------------
     //   Private members
     // ---------------------------------------------
-    var _promoVideos = []; // array to hold reference to all the promo videos
+    var _header = null; // object to hold a refernce for the header component
+    var _promoVideos = []; // array of objects to hold references for the the promo video components
     var _hasFastClickAttached = false; // flag to indicate if fast click was attached
 
     // ---------------------------------------------
@@ -92,7 +94,10 @@ console.log(CONFIG);
         _hasFastClickAttached = true; // set attached flag as true
       }
 
-      // initiate the promo videos
+      // create the header
+      _header = new Header({ element: query(".header")[0] });
+
+      // create the promo videos
       query(".promo-video").forEach(function(element, index){
         _promoVideos.push(new PromoVideo({ element: element }));
       });
@@ -100,7 +105,7 @@ console.log(CONFIG);
 
     // @name destory
     // @desc destory function to destroy the app
-    function destroy() { }
+    function destroy() { /* empty block */ }
 
     // ---------------------------------------------
     //   Constructor block
@@ -127,7 +132,7 @@ console.log(CONFIG);
   ddbWebsite.init(); // initiate the created app
 
 })();
-},{"./base/debounce":2,"./base/print":3,"./base/promise":4,"./base/query":5,"./base/raf":6,"./base/template":7,"./components/promo-video.component":8,"./config":9}],2:[function(require,module,exports){
+},{"./base/debounce":2,"./base/print":3,"./base/promise":4,"./base/query":5,"./base/raf":6,"./base/template":7,"./components/header.component":8,"./components/promo-video.component":9,"./config":10}],2:[function(require,module,exports){
 "use strict";
 
 // -------------------------------------
@@ -693,6 +698,298 @@ require("../base/promise");
 var CONFIG = require("../config");
 
 // -------------------------------------
+//   Component - Header
+// -------------------------------------
+/** 
+  * @name header.component
+  * @desc The header component for the app.
+**/
+
+(function($){
+  console.log("components/header.component.js loaded.");
+
+  /** 
+    * @name Header
+    * @desc the main class for the component
+    * @param {Object} options - options for the component
+    * @return {Object} - the instance of the component class
+  **/
+  function Header(options) {
+    // ---------------------------------------------
+    //   Private members
+    // ---------------------------------------------
+    var _el = { // reference to the DOM elements for
+      main: null, // the main parent DOM element
+      nav:  null, // the header nav  DOM element
+      back: null, // the header back DOM element
+      menu: null  // the header menu DOM element
+    };
+
+    var _class = { // the classes that are contained in
+      main: "header", // the main parent DOM element
+      nav:  "header__nav", // the header nav DOM element
+      back: "header__back", // the header back DOM element
+      menu: "header__menu--mobile", // the header menu DOM element
+
+      modifier: { // applied modifier classes
+        open: "header--open", // when the menu is open
+        active: "link--active", // when the link is active
+        closed: "header--closed" // when the menu is closed
+      }
+    };
+
+    var _elHtml = null; // reference to the html DOM element
+    var _elBody = null; // reference to the body DOM element
+    var _isMenuOpen = false; // flag to indicate if the menu is open
+
+    // ---------------------------------------------
+    //   Public members
+    // ---------------------------------------------
+    /* empy block */
+
+    // ---------------------------------------------
+    //   Private methods
+    // ---------------------------------------------
+    // @name _addOpenClickListener
+    // @desc function to add click event listener for the menu open
+    function _addOpenClickListener() {
+       query("a:first-child", _el.menu)[0].addEventListener("click", open);
+    }
+
+    // @name _addCloseClickListener
+    // @desc function to add click event listener for the menu close
+    function _addCloseClickListener() {
+      query("a:last-child", _el.menu)[0].addEventListener("click", close);
+    }
+
+    // @name _removeOpenClickListener
+    // @desc function to remove click event listener for the menu open
+    function _removeOpenClickListener() {
+      query("a:first-child", _el.menu)[0].removeEventListener("click", open);
+    }
+
+    // @name _removeCloseClickListener
+    // @desc function to remove click event listener for the menu close
+    function _removeCloseClickListener() {
+      query("a:last-child", _el.menu)[0].removeEventListener("click", close);
+    }
+
+    // @name _addWindowResizeListener
+    // @desc function to add the window resize event listener
+    function _addWindowResizeListener() {
+      window.addEventListener("resize", _windowResizeListener);
+    }
+
+    // @name _removeWindowResizeListener
+    // @desc function to remove the window resize event listener
+    function _removeWindowResizeListener() {
+      window.removeEventListener("resize", _windowResizeListener);
+    }
+
+    // @name __windowResizeListener
+    // @desc the listener function for the window resize event
+    function _windowResizeListener(event) {
+      // check if the viewport has been resized beyond the mobile breakpoint
+      if(_isMenuOpen && !(CONFIG.breakpoint.isMobile || CONFIG.breakpoint.isMobileSmall)) {
+        close(); // close the header menu if it is not longer relevant
+      }
+    }
+
+    // @name _isLinkActive
+    // @desc function to check if the given link is active
+    // @param {DOM} link - the link DOM object to be checked for active
+    // @return {Boolean} - the boolean result of the active status check
+    function _isLinkActive(link) { try {
+      var pathName = window.location.pathname;
+      var hrefName = link.getAttribute("href");
+
+      if(hrefName && hrefName.length > 1 
+        && pathName.indexOf(hrefName) != -1) {
+        return true; } 
+
+      else { return false; }} 
+      catch(error) { return false; }
+    }
+
+    // @name __setLinkAsActive
+    // @desc function to set the given link as active
+    function _setLinkAsActive(link) {
+      link.classList.add(_class.modifier.active);
+    }
+
+    // @name __setLinkAsInActive
+    // @desc function to set the given link as inactive
+    function _setLinkAsInactive(link) {
+      link.classList.remove(_class.modifier.active);
+    }
+
+    // ---------------------------------------------
+    //   Public methods
+    // ---------------------------------------------
+    // @name open
+    // @desc function to open the header menu
+    function open(event) {
+      if(_isMenuOpen) {
+        console.log("header.component.js: The header menu is already open.");
+        return false;
+      }
+
+      // prevent default events
+      if(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      // set the menu open flag
+      _isMenuOpen = true;
+
+      // disable scroll on the html and body
+      _elHtml.setAttribute("data-scroll", "disabled");
+      _elBody.setAttribute("data-scroll", "disabled");
+
+      // make sure the nav is hidden
+      // add add, remove the modifiers
+      _el.nav.style.opacity = 0;
+      _el.main.classList.add(_class.modifier.open);
+      _el.main.classList.remove(_class.modifier.closed);
+
+      requestAnimationFrame(function() {
+        // finish any animations currently
+        // being performed on header nav
+        $(_el.nav).velocity("finish");
+
+        // perform the new animation
+        $(_el.nav).velocity("transition.slideUpIn", {
+          easing: "easeInOutQuad", 
+          delay: CONFIG.animation.delay,
+          duration: CONFIG.animation.duration,
+          complete: function() { /* empty block */ }
+        });
+      }); 
+
+      // add the window resize listener
+      _addWindowResizeListener();
+    }
+
+    // @name close
+    // @desc function to close the header menu
+    function close(event) {
+      if(!_isMenuOpen) {
+        console.log("header.component.js: The header menu is already closed.");
+        return false;
+      }
+
+      // prevent default events
+      if(event) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      // reset the menu open flag
+      _isMenuOpen = false;
+
+      // enable scroll on the html and body
+      _elHtml.removeAttribute("data-scroll");
+      _elBody.removeAttribute("data-scroll");
+
+      // make sure the nav is visible
+      // add add, remove the modifiers
+      _el.nav.style.opacity = 1;
+      requestAnimationFrame(function() {
+        setTimeout(function() {
+        _el.main.classList.add(_class.modifier.closed);
+        _el.main.classList.remove(_class.modifier.open);
+        }, CONFIG.animation.delay);
+      });
+
+      requestAnimationFrame(function() {
+        // finish any animations currently
+        // being performed on header nav
+        $(_el.nav).velocity("finish");
+
+        // perform the new animation
+        $(_el.nav).velocity("transition.slideDownOut", {
+          easing: "easeInOutQuad", delay: 0,
+          duration: CONFIG.animation.duration,
+          complete: function() { /* empty block */ }
+        });
+      });
+
+      // remove the window resize listener
+      _removeWindowResizeListener();
+    }
+
+    // ---------------------------------------------
+    //   Constructor block
+    // ---------------------------------------------
+    // check if the header has valid options
+    // element - should be a valid DOM element
+    if(!options || !options.element 
+      || !options.element.nodeName || !options.element.nodeType) {
+      console.log("header.component.js: Cannot create header with invalid options.");
+      return null;  // return null if invalid
+    }
+
+    // get the main parent element
+    _el.main = options.element;
+
+    // get all the child elements
+    _el.nav  = query("." + _class.nav, _el.main)[0];
+    _el.back = query("." + _class.back, _el.main)[0];
+    _el.menu = query("." + _class.menu, _el.main)[0];
+
+    // get the html and body DOM elements
+    _elHtml = query("html")[0];
+    _elBody = query("body")[0];
+
+    // loop through all the nav link and check if any of them are active
+    query("a", _el.nav).forEach(function(link, index) {
+      if(_isLinkActive(link)) { _setLinkAsActive(link); } 
+      else { _setLinkAsInactive(link); }
+    });
+
+    // add click event listeners for 
+    // the menu open and close links
+    _addOpenClickListener();
+    _addCloseClickListener();
+
+    // ---------------------------------------------
+    //   Instance block
+    // ---------------------------------------------
+    return {
+      open: open,   // function to open the header menu
+      close: close  // function to close the header menu
+    };
+  }
+
+  // ---------------------------------------------
+  //   Export block
+  // ---------------------------------------------
+  module.exports = Header;
+
+})(jQuery);
+
+
+},{"../base/promise":4,"../base/query":5,"../config":10}],9:[function(require,module,exports){
+"use strict";
+
+// -------------------------------------
+//   Dependencies
+// -------------------------------------
+/** 
+  * @plugins
+  * require("jquery");
+  * require("velocity");
+**/
+
+// base
+require("../base/query");
+require("../base/promise");
+
+// config
+var CONFIG = require("../config");
+
+// -------------------------------------
 //   Component - Promo Video
 // -------------------------------------
 /** 
@@ -757,6 +1054,11 @@ var CONFIG = require("../config");
 
         // animate the given content into view
         requestAnimationFrame(function() {
+          // finish any animations currently
+          // being performed on the content
+          $(content).velocity("finish");
+
+          // perform the new animation
           $(content).velocity(
             direction == "left" ? 
             "transition.slideLeftIn" : 
@@ -793,6 +1095,11 @@ var CONFIG = require("../config");
 
         // animate the given content out of view
         requestAnimationFrame(function() {
+          // finish any animations currently
+          // being performed on the content
+          $(content).velocity("finish");
+
+          // perform the new animation
           $(content).velocity(
             direction == "right" ? 
             "transition.slideRightOut" : 
@@ -828,7 +1135,7 @@ var CONFIG = require("../config");
       } 
 
       // get the corresponding header contents
-      var elCurrContent = _el.headers[currIndex];
+      var elCurrContent = _el.headers; // get all contents
       var elNextContent = _el.headers[nextIndex];
 
       // animate the current content out of view
@@ -868,13 +1175,17 @@ var CONFIG = require("../config");
 
     // show the next header content 
     // once on initial component load
-    setTimeout(function() {  
-      next(); 
+    requestAnimationFrame(function() {
+      setTimeout(function() {  
+        next(); 
 
-      // and create a loop 
-      // with a set interval
-      setInterval(next, _interval);
-    }, _timeout);
+        // and create a loop 
+        // with a set interval
+        requestAnimationFrame(function() { 
+          setInterval(next, _interval); 
+        });
+      }, _timeout);
+    });
 
     // ---------------------------------------------
     //   Instance block
@@ -893,7 +1204,7 @@ var CONFIG = require("../config");
 })(jQuery);
 
 
-},{"../base/promise":4,"../base/query":5,"../config":9}],9:[function(require,module,exports){
+},{"../base/promise":4,"../base/query":5,"../config":10}],10:[function(require,module,exports){
 "use strict";
 
 // -------------------------------------
